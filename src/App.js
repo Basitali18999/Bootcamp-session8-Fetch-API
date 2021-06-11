4dc3b7d149c940d3d4c31747d9405833a9166998
Basit Ali <basitali18999@gmail.com>
2021-06-11 06:07:09 +0500

    demo Fetched API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+
 
 function App() {
+
+
+
+  const [Fetch, setFetch] = useState([{}]);
+  const [isFetching, setFetching] = useState();
+
+
+
+
+  useEffect(() => {
+
+    async function getData() {
+
+      setFetching(true);
+      const fetchdata = await fetch("https://jsonplaceholder.typicode.com/posts")
+      const data1 = await fetchdata.json()
+      // console.log(data1);
+      const setState = () => {
+        setFetching(false);
+      }
+      setFetch(data1);
+      setTimeout(setState, 4000);
+
+
+
+    }
+    // setTimeout(getData, 3000);
+    getData()
+
+
+
+  }, [])
+
+  if (isFetching) {
+    return (<div><h1>Loading data please wait a moment........</h1></div>)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      
+       <ul>
+        
+        <h2>All title of fetched data</h2>
+        
+        {Fetch.map((names) => {
+
+          return (<li>{names.title}</li>)
+
+        })};
+
+        </ul>
+
+
     </div>
-  );
+
+  )
+
+
 }
+
 
 export default App;
